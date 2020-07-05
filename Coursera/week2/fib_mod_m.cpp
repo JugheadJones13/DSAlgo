baaca94b27403bba1dd4f9737d6cce2df31732a8
JugheadJones13 <balajimurugan980505@gmail.com>
2020-07-05 17:04:21 -0400

    6

--- Coursera/week2/fib_mod_m.cpp
+++ Coursera/week2/fib_mod_m.cpp
@@ -5,9 +5,12 @@ int fib_mod_m(unsigned long long n,unsigned long long m){
 	//find fib(n) mod m;
 	if (n <= 1) return n;
 	vector<unsigned long long> seq = {0,1};
+	//dont actually have to have this fib vector
+	//see fib_ldigit_sum.cpp file in current directory
 	vector<unsigned long long> fib = {0,1};
 	bool done = false;
 	for (int i = 2; i < n +1; i++){
+		//why is putting % m working below?
 		fib.push_back((fib[i-1] + fib[i-2]) % m);
 		if (fib[i] % m == 1 && seq[i-1] == 0) {
 			done = true;
