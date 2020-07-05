a977eb11af5a562bfd738be55954a938c0fc09f3
JugheadJones13 <balajimurugan980505@gmail.com>
2020-07-05 00:31:13 -0400

    fib_mod_m.cpp

---
+++ Coursera/week2/fib_mod_m.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fib_mod_m(unsigned long long n,unsigned long long m){
+	//find fib(n) mod m;
+	if (n <= 1) return n;
+	vector<unsigned long long> seq = {0,1};
+	vector<unsigned long long> fib = {0,1};
+	bool done = false;
+	for (int i = 2; i < n +1; i++){
+		fib.push_back((fib[i-1] + fib[i-2]) % m);
+		if (fib[i] % m == 1 && seq[i-1] == 0) {
+			done = true;
+			seq.pop_back();
+			break;
+		}
+		seq.push_back(fib[i] % m);
+	}
+	int a;
+	if (done){
+		int size = seq.size();
+		a = n % size;
+	}
+	else {
+		a = n;
+	}
+	return seq[a];
+}
+
+int main(){
+	unsigned long long a,b;
+	cin >> a >> b;
+	cout << fib_mod_m(a,b) << endl;
+	return 0;
+}
